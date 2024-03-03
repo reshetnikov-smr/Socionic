5ee15756bde93689a1ad7e963a422a46c489a5d0
D3captain <d3miui@gmail.com>
2024-03-03 14:32:55 +0400

    - remove .idea files from repository - add icon for app

--- app/src/main/java/ru/d3st/socionic/MainActivity.kt
+++ app/src/main/java/ru/d3st/socionic/MainActivity.kt
@@ -32,13 +32,4 @@ class MainActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
-
-    // Shows the system bars by removing all the flags
-// except for the ones that make the content appear under the system bars.
-    private fun showSystemUI() {
-        @Suppress("DEPRECATION")
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
-    }
 }
